00317339d8f2b4e02b56c70b6196ffdcb88adeec
Sean Velasco <seanvelasco@ieee.org>
2024-06-17 20:54:22 +0800

    Initial routes

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func handleLanding(w http.ResponseWriter, r *http.Request) {
+}
+
+func handleAppendOrRetrieveWebResource(w http.ResponseWriter, r *http.Request) {
+}
+
+func handleRetrieveResource(w http.ResponseWriter, r *http.Request) {
+}
+
 func main() {
-	
+	server := &http.Server{
+		Addr: ":8000",
+	}
+	http.HandleFunc("/", handleLanding)
+	http.HandleFunc("/{timestamp}/{resource}", handleRetrieveResource)
+	http.HandleFunc("/{resource}", handleAppendOrRetrieveWebResource)
+
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println(err)
+	}
 }
