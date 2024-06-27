d5177a37023ab1da2c999bfb68359c2c187ff7a9
Sean Velasco <seanvelasco@ieee.org>
2024-06-28 03:27:39 +0800

    Separate handlers

--- handlers.go
+++ handlers.go
@@ -1,1 +1,111 @@
 package main
+
+import (
+	"Goblin/db"
+	"fmt"
+	"github.com/google/uuid"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+)
+
+type WebResource struct {
+	id          uuid.UUID
+	timestamp   time.Time
+	url         string
+	contentType string
+}
+
+func validateUrl(unvalidated string) (string, error) {
+	validated, err := url.ParseRequestURI(unvalidated)
+	if err != nil {
+		if !strings.HasPrefix(unvalidated, "http") {
+			unvalidated = "https://" + unvalidated
+		}
+		validated, err = url.ParseRequestURI(unvalidated)
+		if err != nil {
+			return "", err
+		}
+	}
+	return validated.String(), nil
+}
+
+func archiveResource(url string, resource io.Reader, contentType string) error {
+
+	webResourceObject := WebResource{
+		id:          uuid.New(),
+		timestamp:   time.Now().UTC(),
+		url:         url,
+		contentType: contentType,
+	}
+
+	res, err := db.DB.Exec("INSERT INTO web_resources (id, timestamp, url, content_type) VALUES ($1, $2, $3, $4)",
+		webResourceObject.id,
+		webResourceObject.timestamp,
+		webResourceObject.url,
+		webResourceObject.contentType)
+
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	fmt.Println(res.RowsAffected())
+
+	fmt.Println(webResourceObject)
+	// save to db
+	// save to blob storage
+	return nil
+}
+
+func fetchResource(url string) error {
+
+	res, err := http.Get(url)
+
+	if err != nil {
+		return fmt.Errorf("failed to fetch resource: %w", err)
+	}
+
+	defer res.Body.Close()
+
+	contentType := res.Header.Get("Content-Type")
+
+	if res.StatusCode != http.StatusOK {
+		return fmt.Errorf("invalid status code: %d", res.StatusCode)
+	}
+
+	return archiveResource(url, res.Body, contentType)
+}
+
+func handleLanding(w http.ResponseWriter, r *http.Request) {
+
+	//w.Header().Set("Content-Type", "text/html; charset=utf-8")
+}
+
+func handleAppendOrRetrieveResource(w http.ResponseWriter, r *http.Request) {
+	//resourceUrl := r.PathValue("resource")
+
+	resourceUrl := r.URL.Query().Get("url")
+
+	validUrl, err := validateUrl(resourceUrl)
+
+	if err != nil {
+		http.Error(w, "Invalid URL", http.StatusBadRequest)
+		return
+	}
+	go func() {
+		err := fetchResource(validUrl)
+		if err != nil {
+			log.Printf("Error fetching resource: %v", err)
+		}
+	}()
+
+	w.WriteHeader(http.StatusAccepted)
+	//w.Write([]byte("Resource " + validUrl + " was fetched successfully."))
+}
+
+func handleRetrieveResource(w http.ResponseWriter, r *http.Request) {
+}
--- main.go
+++ main.go
@@ -1,89 +1,28 @@
 package main
 
 import (
-	"fmt"
-	"io"
+	"Goblin/db"
 	"log"
 	"net/http"
-	"net/url"
-	"strconv"
-	"strings"
-	"time"
 )
-
-func validateUrl(unvalidated string) (string, error) {
-	validated, err := url.ParseRequestURI(unvalidated)
-	if err != nil {
-		if !strings.HasPrefix(unvalidated, "http") {
-			unvalidated = "https://" + unvalidated
-		}
-		validated, err = url.ParseRequestURI(unvalidated)
-		if err != nil {
-			return "", err
-		}
-	}
-	return validated.String(), nil
-}
-
-func archiveResource(id string, resource io.Reader) error {
-	timestamp := strconv.FormatInt(time.Now().Unix(), 10)
-	fmt.Println(timestamp)
-	// save to db
-	// save to blob storage
-	return nil
-}
-
-func fetchResource(url string) error {
-
-	res, err := http.Get(url)
-
-	if err != nil {
-		return fmt.Errorf("failed to fetch resource: %w", err)
-	}
-
-	defer res.Body.Close()
-
-	if res.StatusCode != http.StatusOK {
-		return fmt.Errorf("invalid status code: %d", res.StatusCode)
-	}
-
-	return archiveResource(url, res.Body)
-}
-
-func handleLanding(w http.ResponseWriter, r *http.Request) {
-
-	//w.Header().Set("Content-Type", "text/html; charset=utf-8")
-}
-
-func handleAppendOrRetrieveResource(w http.ResponseWriter, r *http.Request) {
-	resourceUrl := r.PathValue("resource")
-	validUrl, err := validateUrl(resourceUrl)
-	if err != nil {
-		http.Error(w, "Invalid URL", http.StatusBadRequest)
-		return
-	}
-	go func() {
-		err := fetchResource(validUrl)
-		if err != nil {
-			log.Printf("Error fetching resource: %v", err)
-		}
-	}()
-
-	w.WriteHeader(http.StatusAccepted)
-	//w.Write([]byte("Resource " + validUrl + " was fetched successfully."))
-}
-
-func handleRetrieveResource(w http.ResponseWriter, r *http.Request) {
-}
 
 func main() {
 
 	server := &http.Server{
 		Addr: ":8000",
 	}
+
 	http.HandleFunc("/", handleLanding)
 	http.HandleFunc("/{timestamp}/{resource}", handleRetrieveResource)
-	http.HandleFunc("/{resource}", handleAppendOrRetrieveResource)
+	http.HandleFunc("/resource", handleAppendOrRetrieveResource)
+
+	if err := db.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	if _, err := db.DB.Exec("CREATE TABLE IF NOT EXISTS web_resources (id TEXT PRIMARY KEY, timestamp TIMESTAMP, url TEXT, content_type TEXT )"); err != nil {
+		log.Fatal(err)
+	}
 
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatal(err)
